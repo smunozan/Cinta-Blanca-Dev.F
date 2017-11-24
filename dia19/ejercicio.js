7ddbe5c0a9b24955b327f11a588fb78be914381a
smunozan <sebasmunoz694@gmail.com>
2017-11-23 22:02:17 -0600

    fin dia

---
+++ dia19/ejercicio.js
@@ -0,0 +1,56 @@
+
+// Input de materialize que almacena el nombre en JS y lo muestra en el html
+// function traerNombre(){
+// 	var nombre = document.getElementById("nombre")
+// 	console.log(nombre.value)
+
+// 	document.getElementById("nombreParaMostrar").innerHTML = nombre.value;
+// }
+
+// --------------------------------
+
+// jalar datos para sumar
+function sumaDosNumeros(){
+	var	num1 = document.getElementById("num1")
+	var	num2 = document.getElementById("num2")
+
+	var suma = Number(num1.value)+Number(num2.value)
+
+	console.log(suma)
+
+	document.getElementById("resultado").innerHTML = suma
+}
+
+function restaDosNumeros(){
+	var	num1 = document.getElementById("num1")
+	var	num2 = document.getElementById("num2")
+
+	var suma = Number(num1.value)-Number(num2.value)
+
+	console.log(suma)
+
+	document.getElementById("resultado").innerHTML = suma
+}
+
+function divisionDosNumeros(){
+	var	num1 = document.getElementById("num1")
+	var	num2 = document.getElementById("num2")
+
+	var suma = Number(num1.value)/Number(num2.value)
+
+	console.log(suma)
+
+	document.getElementById("resultado").innerHTML = suma
+}
+
+function multiplicacionDosNumeros(){
+	var	num1 = document.getElementById("num1")
+	var	num2 = document.getElementById("num2")
+
+	var suma = Number(num1.value)*Number(num2.value)
+
+	console.log(suma)
+
+	document.getElementById("resultado").innerHTML = suma
+}
+
