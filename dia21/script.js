c1b3562e04336d2cb8683e45e458c0db07c41173
Sebastian Munoz <sebasmunoz694@gmail.com>
2017-12-31 08:28:23 -0500

    Prueba

---
+++ dia21/script.js
@@ -0,0 +1,36 @@
+(function(){
+
+})();
+
+'use strict'
+
+
+var personas = [
+	{
+		nombre : "Sebastian",
+		apellido : "Munoz",
+		edad : "23",
+	},
+	{
+		nombre : "Mitzi",
+		apellido : "Diaz",
+		edad : "26",
+	},
+	{
+		nombre : "Angelo",
+		apellido : "luna",
+		edad : "27",
+	},
+	{
+		nombre : "Sol",
+		apellido : "espino",
+		edad : "29",
+	},
+]
+
+persona[0].genero = "m"
+
+// for (var i = 0; i < personas.length; i++) {
+// 	console.log(personas[i].nombre)	
+	
+// }
